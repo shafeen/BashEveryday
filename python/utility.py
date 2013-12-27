7c54f55e7aa281d3f653ea72e922113fb10846fb
Shafeen M. <sodl02@gmail.com>
2013-12-27 13:17:11 -0500

    Update and rename checkFileExistence.py to utility.py

---
+++ python/utility.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os.path
+import subprocess
+
+
+# wrapper for shell commands
+def shell_cmd(command):
+	subprocess.call(command, shell=True)
+	
+
+# query for the existence of a file
+# returns true if exists false otherwise
+# can be made to print output if needed
+def fileExists(filename, printOutput=False, recursive=False):
+	exists = os.path.isfile(filename)
+	if printOutput:
+		print "Checking for filename: \"%s\"" % (filename)
+		message = "File exists" if (exists) else "File doesn't exist"
+		print message
+	return exists
+
+fileExists("utility.py", True)
+fileExists("utility", True)
+fileExists(raw_input("Enter filename to check: "), True)
+	
