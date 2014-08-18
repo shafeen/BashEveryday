49bbbc5361d168e87cfe407f45c362d95c00490e
Shafeen M <sodl02@gmail.com>
2014-08-18 04:46:47 -0400

    updated ubuntu_basic_config

--- puppet/ubuntu_basic_config.pp
+++ puppet/ubuntu_basic_config.pp
@@ -3,7 +3,12 @@
 
 $essentials = [ 'gcc', 'g++', 'vim', 'emacs', 'git', 'openssh-server', 'tmux', 'default-jre' ]
 
+$extras = ['wget', 'lynx', 'default-jdk']
  
 package { $essentials:
+    ensure => 'installed'
+}
+
+package { $extras:
     ensure => 'installed'
 }
